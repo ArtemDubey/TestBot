1db6d8bf31233cc919edaf3097794b6359e6d549
Dand <artem2skazu@gmail.com>
2026-02-27 19:04:35 +0200

    Update logic to Render

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from fastapi import FastAPI
+import uvicorn
 
 BOT_TOKEN = "8557283415:AAEWmvxipGRQsJFaTWn98uR4kQvMA_nWX5U"
+PORT = 8080
+app = FastAPI()
 disp = Dispatcher()
 
 
@@ -12,8 +16,15 @@ disp = Dispatcher()
 async def startMethod(message: Message):
     await message.answer("Bot active")
 
-
+@app.get("/")
+async def check():
+    return {"status":"bot is running"}
 async def main():
     bot = Bot(token=BOT_TOKEN)
 
     await disp.start_polling(bot)
+
+if __name__ == "__main__":
+    result = asyncio.get_event_loop()
+    result.create_task(main())
+    uvicorn.run(app, host = "0.0.0.0", port=PORT)
